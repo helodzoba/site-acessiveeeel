dedbb5ee0fccd8b54de3654cc2a028ea2142d31f
helodzoba <161856879+helodzoba@users.noreply.github.com>
2025-03-10 09:19:41 -0300

    alteração

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(){
-  const aumentaFonteBotao.addEventListener = document.getElementById('aumentar fonte');
+  const aumentaFonteBotao.addEventListener = document.getElementById('aumentar-fonte');
+  const diminuiFonteBotao.addEventListener = document.getElementById('diminui-fonte');
  let tamanhoAtualFonte = 1;
  aumentaFonteBotao. addEventListener('click', function( ){
       tamanhoAtualFonte += 0.1;
