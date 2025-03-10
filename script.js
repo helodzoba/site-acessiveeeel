f3c237a163709e9fbf1de5ec7ef7fdfa9b978a36
helodzoba <161856879+helodzoba@users.noreply.github.com>
2025-03-10 09:07:00 -0300

    diminui

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ document.addEventListener('DOMContentLoaded',function(){
  let tamanhoAtualFonte = 1;
  aumentaFonteBotao. addEventListener('click', function( ){
       tamanhoAtualFonte += 0.1;
-      document.body.style.fontSize = `${tamanhoAtualFonte}`
+      document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+  });
+});
+document.addEventListener('DOMContentLoaded',function(){
+  const diminuiFonteBotao.addEventListener = document.getElementById('aumentar fonte');
+ let tamanhoAtualFonte = 1;
+ diminuiFonteBotao. addEventListener('click', function( ){
+      tamanhoAtualFonte += 0.1;
+      document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
   });
 });
